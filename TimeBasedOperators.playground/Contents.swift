357245ea074cf2fe9753f8591cd658b6c825fefc
blackreo123 <blackreo123@naver.com>
2022-08-16 20:46:34 +0900

    2022-08-16-study replay~

--- TimeBasedOperators.playground/Contents.swift
+++ TimeBasedOperators.playground/Contents.swift
@@ -1,3 +1,151 @@
 import RxSwift
+import RxCocoa
+import UIKit
+import PlaygroundSupport
 
+let disposeBag = DisposeBag()
 
+print("---------- replay ----------")
+let greeting = PublishSubject<String>()
+let parrot = greeting.replay(1)
+parrot.connect()
+
+greeting.onNext("1. hello")
+greeting.onNext("2. hi")
+
+parrot
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+greeting.onNext("2. nice to meet you")
+
+print("---------- replayAll ----------")
+let doctorStrange = PublishSubject<String>()
+let timeStone = doctorStrange.replayAll()
+timeStone.connect()
+
+doctorStrange.onNext("Dormammu")
+doctorStrange.onNext("I've come to bargain")
+
+timeStone
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+//print("---------- buffer ----------")
+//let source = PublishSubject<String>()
+//
+//var count = 0
+//let timer = DispatchSource.makeTimerSource()
+//
+//timer.schedule(deadline: .now() + 2, repeating: .seconds(1))
+//timer.setEventHandler {
+//    count += 1
+//    source.onNext("\(count)")
+//}
+//timer.resume()
+//
+//source
+//    .buffer(timeSpan: .seconds(2), count: 2, scheduler: MainScheduler.instance)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: disposeBag)
+
+//print("---------- window ----------")
+//let maxObservable = 5
+//let makeTime = RxTimeInterval.seconds(2)
+//
+//let window = PublishSubject<String>()
+//
+//var windowCount = 0
+//let windowTimerSource = DispatchSource.makeTimerSource()
+//windowTimerSource.schedule(deadline: .now() + 2, repeating: .seconds(1))
+//windowTimerSource.setEventHandler {
+//    windowCount += 1
+//    window.onNext("\(windowCount)")
+//}
+//windowTimerSource.resume()
+//
+//window
+//    .window(timeSpan: makeTime, count: maxObservable, scheduler: MainScheduler.instance)
+//    .flatMap { windowObservable -> Observable<(index: Int, element: String)> in
+//        return windowObservable.enumerated()
+//    }
+//    .subscribe(onNext: {
+//        print("index: \($0.index), element: \($0.element)")
+//    })
+//    .disposed(by: disposeBag)
+
+//print("---------- delaySubscription ----------")
+//let delaySource = PublishSubject<String>()
+//
+//var delayCount = 0
+//let delayTimeSource = DispatchSource.makeTimerSource()
+//delayTimeSource.schedule(deadline: .now() + 2, repeating: .seconds(1))
+//delayTimeSource.setEventHandler {
+//    delayCount += 1
+//    delaySource.onNext("\(delayCount)")
+//}
+//delayTimeSource.resume()
+//
+//delaySource
+//    .delaySubscription(.seconds(5), scheduler: MainScheduler.instance)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: disposeBag)
+
+//print("---------- delay ----------")
+//let delaySubject = PublishSubject<Int>()
+//var delayCount = 0
+//let delayTimerSource = DispatchSource.makeTimerSource()
+//delayTimerSource.schedule(deadline: .now(), repeating: .seconds(1))
+//delayTimerSource.setEventHandler {
+//    delayCount += 1
+//    delaySubject.onNext(delayCount)
+//}
+//delayTimerSource.resume()
+//
+//delaySubject
+//    .delay(.seconds(3), scheduler: MainScheduler.instance)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: disposeBag)
+
+//print("---------- interval ----------")
+//Observable<Int>
+//    .interval(.seconds(3), scheduler: MainScheduler.instance)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: disposeBag)
+
+//print("---------- timer ----------")
+//Observable<Int>
+//    .timer(.seconds(5), period: .seconds(2), scheduler: MainScheduler.instance)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: disposeBag)
+
+print("---------- timeout ----------")
+let mustPushButton = UIButton(type: .system)
+mustPushButton.setTitle("you must push this button", for: .normal)
+mustPushButton.sizeToFit()
+
+PlaygroundPage.current.liveView = mustPushButton
+
+mustPushButton.rx.tap
+    .do(onNext: {
+        print("tapped")
+    })
+    .timeout(.seconds(5), scheduler: MainScheduler.instance)
+    .subscribe {
+        print($0)
+    }
+    .disposed(by: disposeBag)
